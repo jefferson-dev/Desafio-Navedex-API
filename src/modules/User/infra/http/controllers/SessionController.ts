fe57f00df1134edf05e92edc82cab2549769d65d
Jefferson Marques <dev.jefferson.m@gmail.com>
2021-03-07 12:46:28 -0300

    Correção nos imports do controller Session

--- src/modules/User/infra/http/controllers/SessionController.ts
+++ src/modules/User/infra/http/controllers/SessionController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
-import CreateSession from '@modules/User/Services/CreateSession';
+import CreateSession from '@modules/User/services/CreateSession';
 
 export default class SessionController {
   public async store(request: Request, response: Response): Promise<Response> {
